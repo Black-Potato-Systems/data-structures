d44d67d200e09ad9e3cecb65c48cbc94b129c7f1
kanchan.srivastava <kanchan.srivastava@blackpotato.in>
2024-06-18 14:25:00 +0530

    commiting go project structure

---
+++ internal/dsa/ArraysAndLinkedLists/OneDArray.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+
+	flag.Set("logtostderr", "1")
+	flag.Parse()
+
+	glog.Infoln("Arrays and Linked List .....")
+}
